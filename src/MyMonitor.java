9a81ef84be787398a83aaadf897047628e17d1af
Chester Southwood <csouthwood@eagles.ewu.edu>
2019-07-18 07:58:58 -0700

    Chaned name of job action to serviceRequested for better naming to compare with document.

--- src/MyMonitor.java
+++ src/MyMonitor.java
@@ -12,25 +12,25 @@ public class MyMonitor
 {
 	private class Job 
 	{
-		private String action;
+		private String serviceRequested;
 		private PrintWriter pw;
 		private Socket client;
 		
-		public Job(String action, Socket client) throws IOException
+		public Job(String serviceRequested, Socket client) throws IOException
 		{
-			this.setAction(action);
+			this.setServiceRequested(serviceRequested);
 			this.setClient(client);
 			this.setPrintWriter(new PrintWriter(this.client.getOutputStream(), true)); //printwriter that appends
 		}
 		
-		public String getAction() 
+		public String getServiceRequested() 
 		{
-			return action;
+			return serviceRequested;
 		}
 
-		public void setAction(String action) 
+		public void setServiceRequested(String serviceRequested) 
 		{
-			this.action = action;
+			this.serviceRequested = serviceRequested;
 		}
 
 		public PrintWriter getPrintWriter() 
@@ -62,11 +62,11 @@ public class MyMonitor
 		{
 			try 
 			{
-				this.client.close();
+				this.client.close(); //close socket connection
 			} 
 			catch (IOException e) 
 			{
-				e.printStackTrace();
+				e.printStackTrace(); //shouldn't happen
 			}
 		}
 		
@@ -76,5 +76,150 @@ public class MyMonitor
 			return this.action.toString();
 		}
 	}
+	
+	//took queue class from previous assignment with modifications. Still needs work on it for functionality of this project.
+	
+	private class Node 
+    {
+	   private Object data;
+	   private Node next;
+	   public Node(Object newData) 
+       {
+		   this.data = newData;
+		   this.next = null;
+	   }
+	}  
+	
+	private final int maxSize;
+	private Node head, tail;
+	private int size;
+	
+	public MyMonitor()
+	{
+		this.maxSize = 50;
+		this.size = 0;
+	}
+	
+	public synchronized int getLineCount()
+	{
+		return this.lineCount;
+	}
+	
+	public synchronized void setLineCount(int value)
+	{
+		this.lineCount += value;
+	}
+	
+	public synchronized int getRegexCount()
+	{
+		return this.regexCount;
+	}
+	
+	public synchronized void incrementRegexCount(int value)
+	{
+		this.regexCount += value;
+	}
+	
+	public synchronized boolean getIsDoneSearching()
+	{
+		return this.isDoneSearching;
+	}
+	
+	public synchronized void setIsDoneSearching(boolean value)
+	{
+		this.isDoneSearching = value;
+	}
 
+	public synchronized boolean isEmpty() 
+	{
+		return this.size() == 0;
+	}
+	
+	public synchronized boolean isFull()
+	{
+		return this.size() >= this.maxSize;
+	}
+	 
+	public synchronized int size() 
+	{
+		return this.size;
+	}
+	 
+	public synchronized void enqueue(Object arg0) 
+	{
+		while(this.isFull())
+		{
+			try 
+			{
+				this.wait();
+			} 
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		Node node = new Node(arg0);
+		
+		if (this.isEmpty())
+		{
+			head = node;
+		}
+		else
+		{
+			tail.next = node;  
+		}   
+		
+		tail = node;          
+		size++;
+		
+		notifyAll();
+	}
+	 
+	public synchronized Object dequeue() 
+	{
+		while(this.isEmpty())
+		{
+	         try 
+	         {
+	             wait();
+	         }
+	         catch (InterruptedException e ) 
+	         {
+	             e.printStackTrace();//should stop thread?
+	         }		
+	    }
+		
+		Object data = head.data;
+		Node oldHead = head;
+
+		head = head.next;
+		oldHead.next = null;
+		
+		this.size--;
+		
+		if(this.isEmpty())
+		{
+			tail = null;
+		}
+		
+		notify();
+		
+		return data;
+	}
+	
+	@Override
+	public String toString()
+	{
+		Node tempNode = this.head;
+		String toStr = "";
+		
+		while(tempNode != null)
+		{
+			toStr.concat(" " + (String)tempNode.data);
+			tempNode = tempNode.next;
+		}
+		
+		return toStr;
+	}
 }
